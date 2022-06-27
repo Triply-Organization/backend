e93e7b17dfe29cc89dcbc71efb07e6ca323f126b
phuqthang <thang.phu@nfq.asia>
2022-06-27 18:09:03 +0700

    feat:upload image

---
+++ src/Transformer/BaseTransformer.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Transformer;
+
+use App\Entity\AbstractEntity;
+
+abstract class BaseTransformer
+{
+    public function transform(AbstractEntity $entity, array $params): array
+    {
+        $result = [];
+        foreach ($params as $value) {
+            $getProperty = 'get' . ucfirst($value);
+            if (!method_exists($entity, $getProperty)) {
+                continue;
+            }
+            $result[$value] = $entity->{$getProperty}();
+        }
+        return $result;
+    }
+}
