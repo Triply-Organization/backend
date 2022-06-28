a5558f1b481bc3a09bbb9666ace387edebeb09fd
QuocThangPhu <104897205+QuocThangPhu@users.noreply.github.com>
2022-06-28 11:22:46 +0700

    feat:upload image (#9)
    * feat:upload image

---
+++ src/Controller/Api/ImageController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Controller\Api;
+
+use App\Service\ImageService;
+use App\Traits\ResponseTrait;
+use App\Request\ImageRequest;
+use App\Transformer\ImageTransformer;
+use App\Validator\ImageValidator;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/api/images', name: 'image_')]
+class ImageController extends AbstractController
+{
+    use ResponseTrait;
+
+    #[Route('/', name: 'add_image', methods: 'POST')]
+    public function addImage(
+        Request $request,
+        ImageService $imageService,
+        ImageTransformer $imageTransformer,
+        ImageRequest $imageRequest,
+        ImageValidator $imageValidator
+    ): JsonResponse {
+        $fileRequest = $request->files->get('image');
+        $file = $imageRequest->setImage($fileRequest);
+        $errors = $imageValidator->validatorImageRequest($file);
+        if (!empty($errors)) {
+            return $this->errors($errors);
+        }
+        $image = $imageService->addImage($fileRequest);
+        $results = $imageTransformer->fromArray($image);
+
+        return $this->success($results);
+    }
+}
--- src/Repository/ImageRepository.php
+++ src/Repository/ImageRepository.php
@@ -7,18 +7,15 @@ use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
 
 /**
- * @extends ServiceEntityRepository<Image>
- *
  * @method Image|null find($id, $lockMode = null, $lockVersion = null)
  * @method Image|null findOneBy(array $criteria, array $orderBy = null)
  * @method Image[]    findAll()
  * @method Image[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
  */
-class ImageRepository extends ServiceEntityRepository
+class ImageRepository extends BaseRepository
 {
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, Image::class);
     }
-
 }
