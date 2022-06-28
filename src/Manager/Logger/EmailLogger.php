eff69e18cd6cf1db78db629ec11ae144b8349f90
Nhi Vo <56468937+nhivo1203@users.noreply.github.com>
2022-06-28 17:30:32 +0700

    feat: add Send Email Event (#18)
    * feat: add Send Email Event

---
+++ src/Manager/Logger/EmailLogger.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Manager\Logger;
+
+use PHPMailer\PHPMailer\PHPMailer;
+
+class EmailLogger extends BaseLogger
+{
+    public const SEND = 'SEND EMAIL';
+
+    /**
+     * @param PHPMailer $email
+     */
+    public function emailSend(PHPMailer $email): void
+    {
+        $this->logger->info(self::SEND . ': ');
+        $this->logger->info($this->serializer->serialize($email, 'json'));
+        $this->logger->info('');
+    }
+}
