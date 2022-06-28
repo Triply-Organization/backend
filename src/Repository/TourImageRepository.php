7f1bdcf0b009f5ab873aa6ff579c37cb0b67eb96
phuqthang <thang.phu@nfq.asia>
2022-06-28 18:09:41 +0700

    remove comment code in TourImage

--- src/Repository/TourImageRepository.php
+++ src/Repository/TourImageRepository.php
@@ -14,53 +14,10 @@ use Doctrine\Persistence\ManagerRegistry;
  * @method TourImage[]    findAll()
  * @method TourImage[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
  */
-class TourImageRepository extends ServiceEntityRepository
+class TourImageRepository extends BaseRepository
 {
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, TourImage::class);
     }
-
-    public function add(TourImage $entity, bool $flush = false): void
-    {
-        $this->getEntityManager()->persist($entity);
-
-        if ($flush) {
-            $this->getEntityManager()->flush();
-        }
-    }
-
-    public function remove(TourImage $entity, bool $flush = false): void
-    {
-        $this->getEntityManager()->remove($entity);
-
-        if ($flush) {
-            $this->getEntityManager()->flush();
-        }
-    }
-
-//    /**
-//     * @return TourImage[] Returns an array of TourImage objects
-//     */
-//    public function findByExampleField($value): array
-//    {
-//        return $this->createQueryBuilder('t')
-//            ->andWhere('t.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->orderBy('t.id', 'ASC')
-//            ->setMaxResults(10)
-//            ->getQuery()
-//            ->getResult()
-//        ;
-//    }
-
-//    public function findOneBySomeField($value): ?TourImage
-//    {
-//        return $this->createQueryBuilder('t')
-//            ->andWhere('t.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->getQuery()
-//            ->getOneOrNullResult()
-//        ;
-//    }
 }
