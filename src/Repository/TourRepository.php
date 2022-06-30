66a9434aff8ab514480e1a7759fe5c8601db9fd0
khajackie <khajackie2206@gmail.com>
2022-07-01 00:22:29 +0700

    fix: refactor filter

--- src/Repository/TourRepository.php
+++ src/Repository/TourRepository.php
@@ -5,6 +5,7 @@ namespace App\Repository;
 use App\Entity\Tour;
 use App\Request\ListTourRequest;
 use App\Request\TourRequest;
+use Doctrine\ORM\QueryBuilder;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
 
@@ -28,12 +29,34 @@ class TourRepository extends BaseRepository
     public function getAll(ListTourRequest $listTourRequest): array
     {
         $tours = $this->createQueryBuilder(static::TOUR_ALIAS);
-        $tours = $this->filter($tours, 'maxPeople', $listTourRequest->getGuests());
-        $tours = $this->andFilter($tours, 'id', $listTourRequest->getDestination());
+        $tours = $this->filterGuests($tours, 'maxPeople', $listTourRequest->getGuests());
+        $tours = $this->filterDestination($tours, 'id', $listTourRequest->getDestination());
         $tours = $this->sortBy($tours, $listTourRequest->getOrderType(), $listTourRequest->getOrderBy());
         $tours->setMaxResults($listTourRequest->getLimit())->setFirstResult(ListTourRequest::DEFAULT_OFFSET);
 
         return $tours->getQuery()->getResult();
     }
 
+    protected function filterGuests(QueryBuilder $query, string $field, mixed $value): QueryBuilder
+    {
+        if (empty($value)) {
+            return $query;
+        }
+
+        return $query->where(static::TOUR_ALIAS . ".$field >= :$field")->setParameter($field, $value);
+    }
+
+    protected function filterDestination(QueryBuilder $query, string $field, mixed $value): QueryBuilder
+    {
+        if (empty($value)) {
+            return $query;
+        }
+
+        return $query->innerJoin("App\Entity\TourPlan", "p")
+            ->innerJoin("App\Entity\Destination", "d")
+            ->andWhere(static::TOUR_ALIAS . ".id = p.tour")
+            ->andWhere("p" . ".destination = d.id")
+            ->andWhere("d" . ".$field = :$field")
+            ->setParameter($field, $value);
+    }
 }
