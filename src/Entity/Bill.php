449d250b8351bee5427ad690741ddcc374ce898e
Nhi Vo <56468937+nhivo1203@users.noreply.github.com>
2022-07-02 14:20:17 +0700

    feat: add Checkout API  with Stripe (#25)
    * feat: add Checkout API  with Stripe

--- src/Entity/Bill.php
+++ src/Entity/Bill.php
@@ -8,7 +8,7 @@ use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: BillRepository::class)]
-class Bill
+class Bill extends AbstractEntity
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
