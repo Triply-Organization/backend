d6f4256b10cbdf08655f563f5ebfd8788459c6bd
phuqthang <thang.phu@nfq.asia>
2022-07-04 18:14:50 +0700

    update database migration

--- migrations/Version20220704111424.php
+++ migrations/Version20220704111424.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20220704071412 extends AbstractMigration
+final class Version20220704111424 extends AbstractMigration
 {
     public function getDescription(): string
     {
