2c16a331f53acc7ec790c44a6da9adaa8365b624
phuqthang <thang.phu@nfq.asia>
2022-07-04 12:12:00 +0700

    update data return of Order Tour

--- src/Transformer/OrderTransformer.php
+++ src/Transformer/OrderTransformer.php
@@ -35,11 +35,13 @@ class OrderTransformer extends BaseTransformer
         }
         $ticketsOfOrder = $this->orderService->findTicketsOfOrder($order);
         foreach ($ticketsOfOrder as $key => $ticket) {
-            $result['tickets'][$key]['idTicket'] = $ticket->getId();
-            $result['tickets'][$key]['amount'] = $ticket->getAmount();
-            $result['tickets'][$key]['typeTicket'] = $ticket->getPriceList()->getType()->getName();
-            $result['tickets'][$key]['priceTick'] = $ticket->getPriceList()->getPrice();
+            $ticketInfos[$key]['idTicket'] = $ticket->getId();
+            $ticketInfos[$key]['amount'] = $ticket->getAmount();
+            $ticketInfos[$key]['typeTicket'] = $ticket->getPriceList()->getType()->getName();
+            $ticketInfos[$key]['priceTick'] = $ticket->getPriceList()->getPrice();
         }
+        $result['tickets'] =  $this->ticketInfoReturn($ticketInfos);
+
         $result['discount'] = null;
         if ($order->getDiscount() !== null) {
             $result['discount']['id'] = $order->getDiscount()->getId();
@@ -47,5 +49,32 @@ class OrderTransformer extends BaseTransformer
         }
         $result['subTotal'] = $order->getTotalPrice();
         return $result;
+    }
+
+    private function ticketInfoReturn(array $ticketInfos): array
+    {
+        $arrayResult = [];
+        foreach ($ticketInfos as $ticketInfo) {
+            if ($ticketInfo['typeTicket'] === 'children') {
+                $arrayResult[1]['idTicket'] = $ticketInfo['idTicket'];
+                $arrayResult[1]['amount'] = $ticketInfo['amount'];
+                $arrayResult[1]['typeTicket'] = $ticketInfo['typeTicket'];
+                $arrayResult[1]['priceTick'] = $ticketInfo['priceTick'];
+            }
+            if ($ticketInfo['typeTicket'] === 'young') {
+                $arrayResult[2]['idTicket'] = $ticketInfo['idTicket'];
+                $arrayResult[2]['amount'] = $ticketInfo['amount'];
+                $arrayResult[2]['typeTicket'] = $ticketInfo['typeTicket'];
+                $arrayResult[2]['priceTick'] = $ticketInfo['priceTick'];
+            }
+            if ($ticketInfo['typeTicket'] === 'adult') {
+                $arrayResult[3]['idTicket'] = $ticketInfo['idTicket'];
+                $arrayResult[3]['amount'] = $ticketInfo['amount'];
+                $arrayResult[3]['typeTicket'] = $ticketInfo['typeTicket'];
+                $arrayResult[3]['priceTick'] = $ticketInfo['priceTick'];
+            }
+        }
+
+        return $arrayResult;
     }
 }
