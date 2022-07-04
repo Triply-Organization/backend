d74cac3f141e68896bbed7f59ec580508ba6527e
Nhi Võ <nhivo1604230@gmail.com>
2022-07-04 15:55:29 +0700

    fix: debug

--- src/Service/StripeService.php
+++ src/Service/StripeService.php
@@ -4,8 +4,11 @@ namespace App\Service;
 
 use App\Entity\Bill;
 use App\Repository\BillRepository;
+use App\Repository\OrderRepository;
+use App\Repository\TourRepository;
 use App\Request\CheckoutRequest;
 use PHPMailer\PHPMailer\Exception;
+use Psr\Log\LoggerInterface;
 use Stripe\Checkout\Session;
 use Stripe\Exception\ApiErrorException;
 use Stripe\Stripe;
@@ -21,16 +24,26 @@ class StripeService
     private StripeClient $stripe;
     private BillRepository $billRepository;
     private SendMailService $sendMailService;
+    private TourRepository $tourRepository;
+    private OrderRepository $orderRepository;
+    private LoggerInterface $logger;
 
     public function __construct(
         ParameterBagInterface $params,
-        BillRepository $billRepository,
-        SendMailService $sendMailService
-    ) {
+        BillRepository        $billRepository,
+        SendMailService       $sendMailService,
+        TourRepository        $tourRepository,
+        OrderRepository       $orderRepository,
+        LoggerInterface       $logger
+    )
+    {
         $this->params = $params;
         $this->stripe = new StripeClient($this->params->get('stripe_secret_key'));
         $this->billRepository = $billRepository;
         $this->sendMailService = $sendMailService;
+        $this->tourRepository = $tourRepository;
+        $this->orderRepository = $orderRepository;
+        $this->logger = $logger;
     }
 
     /**
@@ -40,10 +53,10 @@ class StripeService
     {
         $stripeSK = $this->params->get('stripe_secret_key');
         Stripe::setApiKey($stripeSK);
-
+        $languages = '';
         $taxRateId = 'txr_1LGNaYBshpup8grmjONnWYWz';
-
         if ($checkoutRequestData->getCurrency() === 'usd') {
+            $languages = 'en';
             $taxRateId = 'txr_1LGeepBshpup8grmsXrotsGu';
         }
 
@@ -60,10 +73,17 @@ class StripeService
                 'quantity' => 1,
                 'tax_rates' => [$taxRateId]
             ]],
+            'metadata' => [
+                'tour_id' => $checkoutRequestData->getTourId(),
+                'order_id' => $checkoutRequestData->getOrderDetails(),
+            ],
+            'customer_email' => $checkoutRequestData->getEmail(),
+            'locale' => $languages,
+            'submit_type' => 'book',
             'mode' => 'payment',
             'allow_promotion_codes' => true,
             'success_url' => 'http://localhost:3000/confirmation/1',
-            'cancel_url' => 'https://example.com/cancel',
+            'cancel_url' => 'http://localhost:3000/',
         ]);
     }
 
@@ -72,16 +92,21 @@ class StripeService
      */
     public function eventHandler(array $data, string $type): void
     {
-        $bill = new Bill();
+        $bill = new Bill;
         if ($type === self::CHECK_COMPLETED) {
+            $order = $this->orderRepository->find($data['metadata']['order_id']);
+            $tour = $this->tourRepository->find($data['metadata']['tour_id']);
             $bill->setTotalPrice($data['amount_total']);
             $bill->setTax($data['total_details']['amount_tax']);
             $bill->setDiscount($data['total_details']['amount_discount']);
-            $this->billRepository->add($bill, true);
+            $billCreated = $this->billRepository->add($bill, true);
 
             $this->sendMailService->sendBillMail($data['customer_details']['email'], 'Thank you', $bill);
         }
     }
+
+
+
 
     /**
      * @throws ApiErrorException
