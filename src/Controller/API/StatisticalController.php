73193473e5d2d5bc5a62ace1bf40787628fbd19a
Nhi Vo <56468937+nhivo1203@users.noreply.github.com>
2022-07-06 09:54:49 +0700

    Feature/currency converter (#72)
    * feat: add Currency Converter

--- src/Controller/API/StatisticalController.php
+++ src/Controller/API/StatisticalController.php
@@ -5,30 +5,32 @@ namespace App\Controller\API;
 use App\Service\StatisticalService;
 use App\Traits\ResponseTrait;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
 
 #[Route('/api/statistical', name: 'statistical_')]
-class StatisticalController
+class StatisticalController extends AbstractController
 {
     use ResponseTrait;
 
     #[Route('/totalRevenue/{year<\d+>}', name: 'totalRevenue', methods: 'GET')]
     #[IsGranted('ROLE_ADMIN')]
-    public function statisticalTotalRevenue(StatisticalService $statisticalService, $year)
+    public function statisticalTotalRevenue(StatisticalService $statisticalService, $year): JsonResponse
     {
         return $this->success($statisticalService->statisticalTotalRevenue($year));
     }
 
     #[Route('/booking/{year<\d+>}', name: 'booking', methods: 'GET')]
     #[IsGranted('ROLE_ADMIN')]
-    public function statisticalBooking(StatisticalService $statisticalService, $year)
+    public function statisticalBooking(StatisticalService $statisticalService, $year): JsonResponse
     {
         return $this->success($statisticalService->statisticalBooking($year));
     }
 
     #[Route('/', name: 'total', methods: 'GET')]
     #[IsGranted('ROLE_ADMIN')]
-    public function statisticalTotal(StatisticalService $statisticalService)
+    public function statisticalTotal(StatisticalService $statisticalService): JsonResponse
     {
         return $this->success($statisticalService->statisticalTotal());
     }
