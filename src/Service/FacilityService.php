cf4c47e933ac9d42eda2a7126365db4bdac7a342
QuocThangPhu <104897205+QuocThangPhu@users.noreply.github.com>
2022-07-07 00:50:01 +0700

    Feat/get popular tour (#83)
    * feat: getPopularTour

--- src/Service/FacilityService.php
+++ src/Service/FacilityService.php
@@ -2,11 +2,12 @@
 
 namespace App\Service;
 
+use App\Entity\Tour;
 use App\Repository\TourRepository;
-use App\Repository\TourServiceRepository;
 use App\Transformer\ServiceTransformer;
 use App\Transformer\TourServicesTransformer;
 use App\Repository\ServiceRepository;
+use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
 
 class FacilityService
 {
@@ -15,13 +16,15 @@ class FacilityService
     private TourServicesTransformer $tourServicesTransformer;
     private TourRepository $tourRepository;
     private ReviewService $reviewService;
+    private ParameterBagInterface $params;
 
     public function __construct(
         ServiceTransformer      $serviceTransformer,
         ServiceRepository       $serviceRepository,
         TourServicesTransformer $tourServicesTransformer,
         TourRepository          $tourRepository,
-        ReviewService           $reviewService
+        ReviewService           $reviewService,
+        ParameterBagInterface $params,
     )
     {
         $this->serviceTransformer = $serviceTransformer;
@@ -29,17 +32,33 @@ class FacilityService
         $this->tourServicesTransformer = $tourServicesTransformer;
         $this->tourRepository = $tourRepository;
         $this->reviewService = $reviewService;
+        $this->params = $params;
     }
 
     public function getPopularTour()
     {
         $tours = $this->tourRepository->findAll();
-        $ratings = [];
-        foreach ($tours as $key => $tour) {
-            $ratings['tourId'][$key] =$tour->getId();
-            $ratings['rating'][$key] = $this->reviewService->ratingForTour($tour);
+        $ratings = ['rating'];
+        $tourArray = [];
+        $i = 0;
+        foreach ($tours as $tour) {
+            $ratings['rating'][$tour->getId()] = $this->reviewService->ratingForTour($tour);
         }
-       return $ratings;
+        arsort($ratings['rating']);
+
+        foreach ($ratings['rating'] as $key => $rating) {
+            if ($i < 6) {
+                $tourArray[$i] = $this->tourRepository->find($key);
+                $i++;
+            }
+        }
+        foreach ($tourArray as $key => $tour) {
+            $result[$key]['id'] = $tour->getId();
+            $result[$key]['title'] = $tour->getTitle();
+            $result[$key]['image'] = $this->getCoverImage($tour);
+            $result[$key]['rate'] = $this->reviewService->ratingForTour($tour);
+        }
+       return $result;
     }
 
 
@@ -58,6 +77,18 @@ class FacilityService
         $services = $this->serviceRepository->findAll();
         foreach ($services as $service) {
             $result[] = $this->serviceTransformer->toArray($service);
+        }
+
+        return $result;
+    }
+
+    public function getCoverImage(Tour $tour): string
+    {
+        $images = $tour->getTourImages();
+        foreach ($images as $image) {
+            if ($image->getType() === 'cover') {
+                $result = $this->params->get('s3url') . $image->getImage()->getPath();
+            }
         }
 
         return $result;
