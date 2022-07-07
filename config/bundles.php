785c12afd552e2950d3e97de4ec970504569d626
Nhi Võ <nhivo1604230@gmail.com>
2022-07-07 23:53:58 +0700

    fix: checkout bugs

--- config/bundles.php
+++ config/bundles.php
@@ -10,4 +10,6 @@ return [
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
+    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
+    Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
 ];
